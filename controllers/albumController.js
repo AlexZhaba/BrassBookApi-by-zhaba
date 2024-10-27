a16ab6826d387acbe1a25b7bb94fd95f98cadc7c
Alexandr Zhavoronkov <sasazhava13@gmail.com>
2024-10-27 14:13:09 +0300

    add searching filter

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -1,17 +1,35 @@
+import sequilize from "sequelize";
 import { Album } from "../models/Album.js";
 
 export const createAlbum = async (req, res) => {
   const album  = await Album.create({
     user_id: req.user.user_id,
     name: req.body.name,
-    image: `${process.env.IMAGE_HOST}/${req.file.path}`,
+    image: req?.file?.path ? `${process.env.IMAGE_HOST}/${req.file.path}` : '' ,
   })
   res.status(200).json({ album })
 }
 
 export const getAlbums = async (req, res) => {
-  const albums = await Album.findAll({ where: { user_id: req.user.user_id } })
-  res.status(200).json({ albums })
+  const nameFilter = req.query?.name;
+  const searchObject =  { where: { user_id: req.user.user_id } };
+
+  if (nameFilter) {
+    searchObject.where.name = { [sequilize.Op.like]: `%${nameFilter}%` }
+  }
+
+  if (req.query?.offset && req.query?.limit) {
+    searchObject.limit = req.query.limit;
+    searchObject.offset = req.query.offset
+  }
+
+  if (req.query?.sortBy) {
+    const parts = req.query.sortBy.split(':');
+    searchObject.order = [[parts[0], parts[1]]]
+  }
+
+  const { rows: albums, count } = await Album.findAndCountAll(searchObject)
+  res.status(200).json({ albums, count })
 }
 
 export const deleteAlbum = async (req, res) => {
@@ -22,7 +40,7 @@ export const deleteAlbum = async (req, res) => {
 export const updateAlbum = async (req, res) => {
   const album = await Album.update({
     name: req.body.name,
-    image: `${process.env.IMAGE_HOST}/${req.file.path}`,
+    image: req?.file?.path ? `${process.env.IMAGE_HOST}/${req.file.path}` : '',
   }, { where: { id: req.params.id, user_id: req.user.user_id } })
   res.status(200).json({ album })
 }
